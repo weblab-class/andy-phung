1bd3e120c7da8e0bea1eb9d6063bc93afea30db2
Andy <andyphung64@gmail.com>
2024-01-17 14:56:49 -0500

    finish sidebar modals

--- client/src/components/modules/util.js
+++ client/src/components/modules/util.js
@@ -12,7 +12,7 @@ import settings_icon from "../../assets/icons/settings_icon.png";
 // <Modal/>
 const Modal = (props) => {
     return props.visible && (
-        <div className="bg-red-400 absolute ml-auto mr-auto w-[100px] h-[100px]">
+        <div className="bg-white border-black border-4 rounded-3xl absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[600px] h-[350px] z-20">
             {props.content}
         </div>
     )
@@ -21,26 +21,42 @@ const Modal = (props) => {
 // <SideBar/>
 const SideBar = (props) => {
     const sidebarClass = props.isOpen ? "bg-white border-4 border-black top-0 absolute h-full w-[300px] transition-left duration-300 z-20 left-0" : "bg-white border-4 border-black left-[-300px] transition-left duration-300 top-0 absolute h-full w-[300px] z-20";
+
+    const profileModal = <div className="flex flex-col">
+        <img src={close_icon} className="ml-[15px] mt-[15px] mb-[10px] cursor-pointer" width="20" 
+        height="20" onClick={props.closeModal}/>
+        profile modal
+    </div>
+    const achievementsModal = <div className="flex flex-col">
+        <img src={close_icon} className="ml-[15px] mt-[15px] mb-[10px] cursor-pointer" width="20" 
+        height="20" onClick={props.closeModal}/>
+        achievements modal
+    </div>
+    const settingsModal = <div className="flex flex-col">
+        <img src={close_icon} className="ml-[15px] mt-[15px] mb-[10px] cursor-pointer" width="20" 
+        height="20" onClick={props.closeModal}/>
+        settings modal
+    </div>
     
     return (
         <div className={`inline ${sidebarClass}`}>
             <img src={close_icon} className="ml-[17px] mt-[12.5px] mb-[30px] cursor-pointer" width="25" 
             height="25" onClick={props.toggleSidebar}/>
-            <div className="flex items-center pt-[15px] pb-[15px] pl-[20px] cursor-pointer hover:bg-gray-300">
+            <div onClick={() => {props.openModal(profileModal)}} className="flex items-center pt-[15px] pb-[15px] pl-[20px] cursor-pointer hover:bg-gray-300">
                 <img src={profile_icon} className="" width="45px"  
                 height="45px"/>
                 <div className="ml-[11px] text-[25px]">
                     Profile
                 </div>
             </div>
-            <div className="flex items-center pt-[15px] pb-[15px] pl-[20px] cursor-pointer hover:bg-gray-300">
+            <div onClick={() => {props.openModal(achievementsModal)}} className="flex items-center pt-[15px] pb-[15px] pl-[20px] cursor-pointer hover:bg-gray-300">
                 <img src={achievements_icon} className="" width="45px"  
                 height="45px"/>
                 <div className="ml-[11px] text-[25px]">
                     Achievements
                 </div>
             </div>
-            <div className="flex items-center pt-[15px] pb-[15px] pl-[20px] cursor-pointer hover:bg-gray-300">
+            <div onClick={() => {props.openModal(settingsModal)}} className="flex items-center pt-[15px] pb-[15px] pl-[20px] cursor-pointer hover:bg-gray-300">
                 <img src={settings_icon} className="" width="45px"  
                 height="45px"/>
                 <div className="ml-[11px] text-[25px]">
@@ -54,16 +70,23 @@ const SideBar = (props) => {
 // <NavBar/>
 const NavBar = (props) => {
     const [sideBarOpen, setSideBarOpen] = useState(false);
-    const [modalOpen, setModalOpen] = useState(false); // idk what im doing
-    // TODO: rework how u impl and use modals
-
-    const handleModalToggle = () => {
-        setModalOpen(!modalOpen);
-    }
+    const [modalOpen, setModalOpen] = useState(false);
+    const [modalContent, setModalContent] = useState(<></>); 
 
     const handleSidebarClick = () => {
         setSideBarOpen(!sideBarOpen);
     };
+
+    const openModal = (content) => {
+        setModalOpen(true);
+        setModalContent(content);
+    }
+    
+    const closeModal = () => {
+        setModalOpen(false);
+        setModalContent(<></>);
+    }
+    
 
     return props.visible && (
         <>
@@ -74,8 +97,8 @@ const NavBar = (props) => {
                     purrductive
                 </div>
             </div>
-            <SideBar isOpen={sideBarOpen} toggleSidebar={handleSidebarClick} handleModalToggle={handleModalToggle}/>
-
+            <SideBar isOpen={sideBarOpen} toggleSidebar={handleSidebarClick} openModal={openModal} closeModal={closeModal}/>
+            <Modal visible={modalOpen} content={modalContent}/>
         </>
         
     )
